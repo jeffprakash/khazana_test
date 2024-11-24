66102f080c98fbbc4e8cb4fc185aae30055e1b57
jeffprakash <jeffprakash@gmail.com>
2024-11-24 13:23:11 +0530

    done

--- app/models.py
+++ app/models.py
@@ -10,14 +10,22 @@ class User(db.Model):
     monthly_income = db.Column(db.Float, nullable=True)
     role = db.Column(db.String(50), default='user')
 
+    __table_args__ = (
+        db.Index('idx_user_email', 'email'),
+    )
+
+
 class Goal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     title = db.Column(db.String(120), nullable=False)
     target_amount = db.Column(db.Float, nullable=False)
-    current_savings = db.Column(db.Float, nullable=False)
+    current_savings = db.Column(db.Float, nullable=False,default=0)
     target_date = db.Column(db.Date, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user = db.relationship('User', backref=db.backref('goals', lazy=True, cascade="all, delete"))
+
 
 class Portfolio(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -32,8 +40,15 @@ class Asset(db.Model):
     amount_invested = db.Column(db.Float, nullable=False)
     purchase_date = db.Column(db.Date, nullable=False)
 
+    portfolio = db.relationship('Portfolio', backref=db.backref('assets', lazy=True))
+
+
 class PriceHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     asset_id = db.Column(db.Integer, db.ForeignKey('asset.id'), nullable=False)
     date = db.Column(db.Date, nullable=False)
     price = db.Column(db.Float, nullable=False)
+
+
+    asset = db.relationship('Asset', backref=db.backref('price_history', lazy=True))
+
